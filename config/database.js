96253ef7fe58cad2141e5b5b94ac69f9d2d63137
Cyrille JINDJ <cjindjou-ext@nexity.fr>
2026-02-19 09:38:59 +0100

    chore: Update Database to use env.

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const {
-  DB_HOST = 'localhost',
-  DB_PORT = '5432',
-  DB_NAME = 'adoptapet',
-  DB_USER = 'postgres',
-  DB_PASSWORD = 'postgres'
+  DB_HOST = "localhost",
+  DB_PORT = "5432",
+  DB_NAME = "adoptapet",
+  DB_USER = "adoptapet",
+  DB_PASSWORD = "adoptapet",
 } = process.env;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: Number(DB_PORT),
-  dialect: 'postgres',
-  logging: false
+  dialect: "postgres",
+  logging: false,
 });
 
 module.exports = sequelize;
